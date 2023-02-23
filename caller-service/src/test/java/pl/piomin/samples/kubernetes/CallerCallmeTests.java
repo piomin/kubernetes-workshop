e9b0283df5cc06c22342bdcb2168fdf56e59285d
piomin <piotr.minkowski@gmail.com>
2023-02-23 23:25:54 +0100

    enable auto-update

---
+++ caller-service/src/test/java/pl/piomin/samples/kubernetes/CallerCallmeTests.java
@@ -0,0 +1,32 @@
+package pl.piomin.samples.kubernetes;
+import io.specto.hoverfly.junit.core.Hoverfly;
+import io.specto.hoverfly.junit5.HoverflyExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
+import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
+
+@SpringBootTest(properties = {"VERSION = v2", "POD_NAME = test", "POD_NAMESPACE = default"}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(HoverflyExtension.class)
+public class CallerCallmeTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void callmeIntegration(Hoverfly hoverfly) {
+        hoverfly.simulate(
+                dsl(service("http://callme-service.pminkows-serverless.svc.cluster.local")
+                        .get("/callme/ping")
+                        .willReturn(success().body("I'm callme-service v1.")))
+        );
+        String response = restTemplate.getForObject("/caller/ping", String.class);
+        assertEquals("caller-service(null): test in default", response);
+    }
+}
